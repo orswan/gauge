9c5bdfea67bc2210a3e2ee3186d51cd02f1f4f07
Hunter Swan <orswan@stanford.edu>
2016-03-09 17:04:12 -0800

    fixing bug in gaugedata.py that made it not do ZN for N>2

--- gaugedata.py
+++ gaugedata.py
@@ -9,16 +9,16 @@ for n in range(2,5):		# n==number of dimensions
 	ndata = []
 	print("Dimension {}".format(n))
 	# ZN with U1 action
-	for N in range(7):		# N==order of the group
+	for N in range(2,7):		# N==order of the group
 		print('Doing U1 Z{}'.format(N))
-		FT = Field((7,)*n,*ZN(2,'U1'),0,init='rand')
+		FT = Field((7,)*n,*ZN(N,'U1'),0,init='rand')
 		en,sd,bs = hyst(FT,(0,3,.01*n),neq=5,nstat=60//n,relax=20//n,inc=1)
 		ndata.append( ('{},{} with U1 action: en,sd,bs'.format(n,N),en,sd,bs) )
 	
 	# ZN with delta action
-	for N in range(1,7):
+	for N in range(2,7):
 		print('Doing delta Z{}'.format(N))
-		FT = Field((7,)*n,*ZN(2,'delta'),0,init='rand')
+		FT = Field((7,)*n,*ZN(N,'delta'),0,init='rand')
 		en,sd,bs = hyst(FT,(0,3,.01*n),neq=5,nstat=60//n,relax=20//n,inc=1)
 		ndata.append( ('{},{} with delta action: en,sd,bs'.format(n,N),en,sd,bs) )
 	
